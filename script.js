78d983e0435bbe6a84077bf6b3a987f8b9f4802c
Cami-Iglesias <112730894+Cami-Iglesias@users.noreply.github.com>
2023-03-03 16:16:34 -0300

    arreglo de estilo seccion contacto
    cambie los tamaños para que se vea mejor esteticamente y agregue un hobbie manualidades.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+/*
+const iconPink = () => {
+    const i = document.createElement("i");
+    i.classList.add("fa-solid", "fa-envelope", "contacto__boton--img");
+    i.addEventListener("focus", iconMensaje);
+}
+*/
